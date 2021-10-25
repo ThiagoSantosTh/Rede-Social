0e5f322808b41790134422e4122367d31e8a21a8
LAPTOP-H02F1CNN\thiag <thiagosantosab070@gmai.com>
2021-10-25 06:54:17 -0300

    Rede social - Para animais

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'animais',
+    loadChildren: () =>
+      import('./animais/animais.module').then((m) => m.AnimaisModule),
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/app/home/home.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MensagemModule } from '../componentes/mensagem/mensagem.module';
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
+
+@NgModule({
+  declarations: [HomeComponent, LoginComponent, NovoUsuarioComponent],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    FormsModule,
+    MensagemModule,
+    ReactiveFormsModule,
+  ],
+  exports: [HomeComponent],
+})
+export class HomeModule {}
